604a3b006c256be3a51eab2a7291087764fc86e4
imobileweb <alan@imobileweb.com.br>
2023-04-06 21:29:10 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>fornecedor</h3>
+
+{{ 'Texto de teste'}}
+<?= 'texto de teste' ?>
+
+{{-- Fica o comentáro que será descartado pelo interpretador do blade --}}
+
+@php 
+// para comentar uma linha
+
+/*
+para comentarios de multiplas linhas
+*/
+
+echo 'Texto de teste';
+@endphp
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return 'Ola, até que enfim';
+});
+*/
+
+Route::get('/','PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@SobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+//nome,categoria,assunto,mensagem
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir a pagina inicial';
+});
