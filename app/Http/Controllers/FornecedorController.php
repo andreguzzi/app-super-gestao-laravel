70657b91c0490438a3b0c09fc238db77f3bbe336
andreguzzi <andre_guzzi@hotmail.com>
2023-04-25 22:44:49 -0300

    listar e editar fornecedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,10 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
+       }
+
+       public function listar(Request $request){
+
+        $fornecedores = Fornecedor::where('nome','like','%'.$request->input('nome').'%')
+        ->where('site','like','%'.$request->input('site').'%')
+        ->where('uf','like','%'.$request->input('uf').'%')
+        ->where('email','like','%'.$request->input('email').'%')
+        ->get();
+
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
+       }
+
+       public function adicionar(Request $request){
+
+        $msg = '';
+
+        if($request->input('_token') != '') {
+            //validacao
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no minimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no maximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no minimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no maximo 2 caracteres',
+                'email.email' => 'O campo e-mail não foi preenchido corretamente'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            //redirect
+
+            //dados view
+            $msg = 'Cadastro realizado com sucesso';
+        }
+
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
        }
 }
